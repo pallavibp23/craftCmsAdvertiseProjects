28aa3adc2effa3175a21e43b956779ec4497b712
pallavi <pallavi.bp23@gmail.com>
2026-02-13 04:09:00 +0000

    Initial commit

---
+++ config/custom.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Custom Configuration
+ *
+ * Settings that are not part of GeneralConfig go here.
+ * For example: allowedHosts, custom feature flags, etc.
+ */
+
+return [
+    // Allowed hosts for the site
+    'allowedHosts' => [
+        '.trycloudflare.com',
+        '.ddev.site',
+    ],
+
+    // Example: custom feature flag
+    // 'enableBetaFeatureX' => true,
+];
