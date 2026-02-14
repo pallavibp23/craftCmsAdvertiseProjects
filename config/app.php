0c5ec812217a19aa86a0d0d1f234e20bb4be8026
pallavi <pallavi.bp23@gmail.com>
2026-02-14 13:12:15 +0000

    Add environment example file

--- config/app.php
+++ config/app.php
@@ -1,14 +1,27 @@
 <?php
 
 use craft\helpers\App;
+use Craft;
 
 return [
     'id' => App::env('CRAFT_APP_ID') ?: 'CraftCMS',
 
+    // Environment-specific settings
+    'bootstrap' => App::env('CRAFT_ENVIRONMENT') === 'production' 
+        ? []   // Disable all plugins on production/Cloud
+        : null, // Keep plugins enabled locally
+
     'components' => [
+        // Use file cache instead of DB cache in production
         'cache' => function () {
+            if (App::env('CRAFT_ENVIRONMENT') === 'production') {
+                return Craft::createObject([
+                    'class' => \yii\caching\FileCache::class,
+                ]);
+            }
+            // Default cache for local/dev
             return Craft::createObject([
-                'class' => \yii\caching\FileCache::class,
+                'class' => \craft\cache\DbCache::class,
             ]);
         },
     ],
